e50378bc0dcc362d1d62581fe8ad561111043a1f
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-12-08 03:51:13 -0400

    add packages

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,11 +6,18 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-# pacman -Syu --noconfirm PACKAGESHERE
+pacman -Syu --noconfirm  \
+	libxcb           \
+	libxcursor       \
+	libxkbcommon-x11 \
+	libxrandr        \
+	libxtst          \
+	mesa-utils       \
+	vulkan-tools     \
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
-get-debloated-pkgs --add-common --prefer-nano
+get-debloated-pkgs --add-mesa
 
 # Comment this out if you need an AUR package
 #make-aur-package PACKAGENAME
